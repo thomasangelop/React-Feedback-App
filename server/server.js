504ed4ab3faa71d84a0465477515b19b96c8275c
Thomas Angelo Pinataro <tapinataro@csbsju.edu>
2018-11-07 19:55:17 -0600

    database working on PORT 5000

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
